f6e8ef3473ad154f1ec1a5e856614a78c18c93b5
김뚜루 <111275210+w0nderland05@users.noreply.github.com>
2023-04-08 23:18:50 +0900

    Study Entity

---
+++ src/main/java/org/study/entities/Study.java
@@ -0,0 +1,54 @@
+package org.study.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.study.commons.constants.RegionType;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Study extends BaseEntity {
+
+
+    @Id
+    @GeneratedValue
+    private Long studyCode; //스터디코드
+
+    @Column(nullable = false, length = 150)
+    private String studyNm; //스터디명
+
+    @Column(nullable = false)
+    private String category; //카테고리
+
+    private LocalDateTime requestDt;//개설신청일시
+
+
+    @Column(nullable = false)
+    private boolean approveStatus; //승인상태
+
+    private LocalDateTime regStatusDt;//상태처리일시
+
+    private Long maxMember; //신청최대인원수
+
+    private Long numOfWeek; //스터디 주당횟수
+
+    @Enumerated(EnumType.STRING)
+    @Column(length=30, nullable=false)
+    private  RegionType regionType = RegionType.ONLINE; //지역(온라인/ 오프라인 -추후 프로그램내에서 시/도/군/구 값도 추가되도록 진행예정 )
+
+    @Column(length = 150)
+    private String simpleIntro; //한줄 소개글
+
+    @Lob
+    @Column(nullable = false)
+    private String Introduction; //소개글
+
+    @ManyToOne
+    @JoinColumn(name="user_No")
+    @ToString.Exclude
+    private User user;
+}
