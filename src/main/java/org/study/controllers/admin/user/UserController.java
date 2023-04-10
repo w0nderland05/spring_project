8a28c310f173f85f1348d56fcd2501e268c267d9
82everywin <82everywin@gmail.com>
2023-04-11 05:21:22 +0900

    cs 컨트롤러 재업

--- src/main/java/org/study/controllers/admin/user/UserController.java
+++ src/main/java/org/study/controllers/admin/user/UserController.java
@@ -1,6 +1,8 @@
 package org.study.controllers.admin.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/admin/user")
@@ -8,7 +10,7 @@ public class UserController {
 
     /**
      * <회원관리> 클릭시 나오는 페이지
-     * == 회원 목록이 보임
+     * == 회원 목록
      *
      * @return
      */
@@ -16,6 +18,7 @@ public class UserController {
     public String index() {
         return "admin/user/index";
     }
+
 
     /**
      * 회원 정보 상세
@@ -26,5 +29,20 @@ public class UserController {
     public String detail(){
         return "admin/user/detail";
 
+    }
+
+    /**
+     * 회원 목록에서 '관리' 버튼 클릭시 나오는 페이지
+     * 하단의 탈퇴/정지 버튼 클릭시 나오는 페이지
+     * : 코드 302 로 페이지 임시이동 예정
+     *
+     * @param userNo
+     * @return
+     */
+    @PostMapping("/manage")
+    public String manage(@PathVariable Long userNo){
+
+        // 이 부분 좀 더 생각해보고, 수정 바랍니다.
+        return "redirect: admin/user";
     }
 }
