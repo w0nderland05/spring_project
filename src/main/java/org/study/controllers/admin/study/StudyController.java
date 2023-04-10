f6593a3c5160dfd62338b9c6870a845541876162
82everywin <82everywin@gmail.com>
2023-04-11 05:20:43 +0900

    admin/study 컨트롤러, html 추가

--- src/main/java/org/study/controllers/admin/study/StudyController.java
+++ src/main/java/org/study/controllers/admin/study/StudyController.java
@@ -1,4 +1,43 @@
 package org.study.controllers.admin.study;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/study")
 public class StudyController {
+
+    /**
+     * <스터디관리> 클릭시 나오는 페이지
+     * ==스터디 목록
+     *
+     * @return
+     */
+    @GetMapping
+    public String index(){
+        return "admin/study/index";
+    }
+
+    /**
+     * 스터디 개설 신청 관리
+     *
+     * @return
+     */
+    @GetMapping("/approvals")
+    public String approvals(){
+        return "admin/study/approvals";
+    }
+
+    /**
+     * 카테고리 관리
+     *
+     * @return
+     */
+    @GetMapping("/category")
+    public String category(){
+        return "admin/study/category";
+    }
+    
+
 }
