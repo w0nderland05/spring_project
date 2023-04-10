6fb4798bb15ba50d34192e7d689d0836c98785e7
82everywin <82everywin@gmail.com>
2023-04-10 17:54:00 +0900

    community  import수정

--- src/main/java/org/study/entities/Community.java
+++ src/main/java/org/study/entities/Community.java
@@ -42,11 +42,10 @@ public class Community extends BaseEntity {
     private boolean reply; // 댓글사용
 
     private boolean movement; // 글 작성 후 이동
-<<<<<<< HEAD
-=======
+
 
     @ManyToOne
     @JoinColumn(name = "userNo")
     private User user;
->>>>>>> 755442685748a07458b19984865ee2bef7960aa0
+
 }
