7d49177617c94a7fd41c9b7b280fa107b432e0be
82everywin <82everywin@gmail.com>
2023-04-11 17:10:25 +0900

    error 기능 추가

--- src/main/java/org/study/controllers/commons/ErrorsController.java
+++ src/main/java/org/study/controllers/commons/ErrorsController.java
@@ -1,22 +1,27 @@
 package org.study.controllers.commons;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/error")
 public class ErrorsController {
+
     /**
      * 권한없음 상태 오류 페이지
      * 
      * @return
      */
     @GetMapping("/401")
-    public String status401() {
-
+    public String status401(HttpServletResponse response, Model model) {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        model.addAttribute("status",401);
         return "error/401";
     }
+
 
     /**
      * 페이지 없음 오류 페이지
@@ -24,7 +29,9 @@ public class ErrorsController {
      * @return
      */
     @GetMapping("/404")
-    public String status404(){
+    public String status404(HttpServletResponse response,Model model){
+        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        model.addAttribute("status",404);
         return "error/404";
     }
 }
