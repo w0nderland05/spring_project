73e754d9ba5a878a1e9c11634752daba45c96cd8
kookong2 <hy010309@naver.com>
2023-04-14 14:01:11 +0900

    주석 추가
    주석

--- src/main/java/org/study/entities/UserSub.java
+++ src/main/java/org/study/entities/UserSub.java
@@ -3,5 +3,12 @@ package org.study.entities;
 
 
 public class UserSub {
-
+    /**
+     * 아이디 값은 어떻게 할건지?
+     */
+    /**
+     * 추가해야하는 부분 ( !!중요!! UserInfo랑 UserInfoService 수정 !!중요!!)
+     * ++ 이용제한은 UserSaveService에도 추가해야함
+     * 탈퇴일자, 이용제한 ( 만약 라디오로 할거면 Enum으로, 캘린더로 할거면 이건 강사님한테 물어보던가 구글링 )
+     */
 }
