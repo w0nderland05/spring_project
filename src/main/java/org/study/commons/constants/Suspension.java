37393b715356d3621e03b4cf713b0ab15e69236a
kookong2 <hy010309@naver.com>
2023-04-14 11:24:00 +0900

    UserSub 엔티티 추가
    .

---
+++ src/main/java/org/study/commons/constants/Suspension.java
@@ -0,0 +1,11 @@
+package org.study.commons.constants;
+
+public enum Suspension {
+
+    USING,      // 사용중
+    THREE,      // 3일 정지
+    SEVEN,      // 7일 정지
+    THIRTY,     // 30일 정지
+    YEAR,       // 1년 정지
+    BAN         // 영구정지
+}
---
+++ src/main/java/org/study/entities/UserSub.java
@@ -0,0 +1,7 @@
+package org.study.entities;
+
+
+
+public class UserSub {
+
+}
