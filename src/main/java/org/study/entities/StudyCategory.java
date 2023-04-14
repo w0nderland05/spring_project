c60bcba6de5d07b7c56175724a0776fe18179d82
yonggyo1125 <yonggyo00@kakao.com>
2023-04-15 01:37:09 +0900

    프론트 공통 상단 작업

--- src/main/java/org/study/entities/StudyCategory.java
+++ src/main/java/org/study/entities/StudyCategory.java
@@ -2,7 +2,6 @@ package org.study.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
