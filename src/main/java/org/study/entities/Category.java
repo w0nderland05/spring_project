2a14d1f5749253401f613800f599992909fe86e4
yonggyo1125 <yonggyo00@kakao.com>
2023-04-16 01:11:08 +0900

    카테고리 엔티티 수정

--- src/main/java/org/study/entities/Category.java
+++ src/main/java/org/study/entities/Category.java
@@ -1,17 +1,22 @@
 package org.study.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 /**
  * 범용 분류 entity
  */
 @Entity
-public class Category {
+@Table(indexes={
+        @Index(name="idx_listOrder_desc", columnList="listOrder DESC"),
+        @Index(name="created_at_desc", columnList="createdAt DESC")
+})
+public class Category extends BaseEntity {
 
     @Id @Column(length=20)
     private String cateCd; // 분류 코드
+
+    @Column(name="is_use")
+    private boolean use; // 사용 여부
     @Column(length=30, nullable = false)
     private String location; // 노출 위치
 
@@ -20,5 +25,5 @@ public class Category {
     private Long listOrder; // 진열 순서 - 숫자가 높을수록 진열 우선 가중치 부여
 
     @Column(length=20)
-    private String parentCateCd; // 하위 분류인 경우는 부모 분류코드가 있음
+    private String parentCateCd; // 하위 분류인 경우는 부모 분류코드가 있
 }
