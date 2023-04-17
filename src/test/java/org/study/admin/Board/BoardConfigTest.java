cbbce0db07ff43cbfd2bb1dbf6c8f38d236347a9
82everywin <82everywin@gmail.com>
2023-04-18 04:39:58 +0900

    게시판 등록 테스트 메서드 추가

---
+++ src/test/java/org/study/admin/Board/BoardConfigTest.java
@@ -0,0 +1,31 @@
+package org.study.admin.Board;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * '게시판 관리 - 게시판 등록'에 해당하는 테스트 클래스 입니다.
+ * 파일명 : "BoardConfigService"
+ * 메서드 : config()
+ *
+ */
+public class BoardConfigTest {
+
+    @Test
+    @DisplayName("게시판 등록 성공시 True값 반환")
+    void ConfigSuccess(){
+
+    }
+
+    @Test
+    @DisplayName("Config Null값일때 BoardConfigFailException 발생")
+    void BoardConfig_Null_Exception(){
+
+    }
+
+    @Test
+    @DisplayName("필수 입력 값 체크 - BoardConfigFailException 발생")
+
+
+
+}
