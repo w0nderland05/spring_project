f062e790bcc823f1bc25a974875a5516db70b928
82everywin <82everywin@gmail.com>
2023-04-18 04:38:32 +0900

    신고사유관리 테스트 메서드 추가

---
+++ src/test/java/org/study/admin/Cs/CsReasonTest.java
@@ -0,0 +1,15 @@
+package org.study.admin.Cs;
+
+/**
+ * '신고 사유 관리'에 해당하는 테스트 입니다.
+ * 파일명 : "ReportReason_Service"
+ * Model : Category를 통해서 사유관리를 가져 올 수 있습니다.
+ * ex)  cateCd: Report
+ *      use:yes
+ *      location: 노출위치 질문..
+ *      cateNm: 신고사유관리
+ *
+ */
+public class CsReasonTest {
+
+}
