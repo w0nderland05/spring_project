4f3410f5afd12e7c7a3c9e6a311716e28fce7020
DESKTOP-V7L95BH\windows10 <xodls713@naver.com>
2023-04-19 01:10:07 +0900

    수정

--- src/main/java/org/study/controllers/user/Community/CommunityController.java
+++
@@ -1,4 +0,0 @@
-package org.study.controllers.user.Community;
-
-public class CommunityController {
-}
--- src/main/java/org/study/controllers/user/Community/UserCommunityController.java
+++ src/main/java/org/study/controllers/user/Community/UserCommunityController.java
@@ -1,10 +1,10 @@
-package org.study.controllers.user;
+package org.study.controllers.user.Community;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class CommunityController {
+public class UserCommunityController {
     /**
      * <커뮤니티> 클릭하면 나오는 페이지
      *
