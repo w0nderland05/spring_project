8e47f3c67dfa1a0767086245256b88dd03cb82c7
김뚜루 <111275210+w0nderland05@users.noreply.github.com>
2023-04-20 22:59:10 +0900

    작업중

--- src/test/java/org/study/admin/study/StudyApplyTest.java
+++ src/test/java/org/study/admin/study/StudyApplyTest.java
@@ -4,52 +4,48 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(locations="classpath:application-test.properties")
+@TestPropertySource(locations = "classpath:application-test.properties")
 @ExtendWith(MockitoExtension.class)
 public class StudyApplyTest {
-    /**
-    @Autowired
-    private StudyRepository studyRepository;
-
-    @Autowired
-    private StudyCategoryRepository scRepository;
-
-    // private final StudyApplyService applyService;
-    @Autowired
-    private StudyApplyService applyService;
 
 
-    /**
-     * @Test Case - StudyApplyService::apply
-     *
+
+      @Autowired
+      private StudyApplyService applyService;
+
+     /** @Test Case - StudyApplyService::apply
+     * <p>
      * 스터디 신청이 되면 관리자페이지의 승인을 거치게 되며
      * 승인이라는 상태 값을 통해서 신청 상태가 관리가 됩니다.
-     *
+     * <p>
      * 테스트는 등록과 승인이라는 측면에서 진행되며 기능이 구현됩니다.
      * 1. 등록에서의 최종 목표는 예외 발생 없이 등록이 되는 것
      * 2. 제대로 등록하기 위한 유효성 검사 항목이 다음과 같이 정리됩니다.
      */
-    /**
 
-    /** 단위 테스트 S */
+     /**
+     * 단위 테스트 S
+     */
 
     // 단위 테스트는 구현된 Service가 정상 동작하는지 단위별로 테스트 합니다.
     // junit5의 단언(assert)와 mockito를 주로 사용하게 됩니다.
-
     @Test
     @DisplayName("스터디 개설 신청이 완료되면 예외가 발생하지 않음(최종 목적)")
     public void applySuccess() {
+
+
+
     }
 
     @Test
     @DisplayName("스터디 신청 필수 항목 유효성 검사 - 카테고리, 한줄 소개글, 소개내용, 메인 사진")
-    public void applyValidationTest(){
+    public void applyValidationTest() {
         // 카테고리는 나중에 검사
-
 
 
     }
@@ -61,14 +57,13 @@ public class StudyApplyTest {
      */
     @Test
     @DisplayName("스터디 신청 데이터와 DB 데이터의 일치 여부 체크")
-    public void assertApplyToDB(){
+    public void assertApplyToDB() {
 
 
     }
 
 
     /** 단위 테스트 E */
-
 
 
 }
