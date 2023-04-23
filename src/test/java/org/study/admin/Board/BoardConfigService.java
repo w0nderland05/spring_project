3a0f6470c519b42d8b59dc8e6abd8994988e1cef
7aein96 <xodls713@naver.com>
2023-04-24 00:48:01 +0900

    수정

--- src/test/java/org/study/admin/Board/BoardConfigService.java
+++ src/test/java/org/study/admin/Board/BoardConfigService.java
@@ -2,10 +2,12 @@ package org.study.admin.Board;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.Errors;
 import org.study.commons.constants.board.AfterWriteTarget;
 import org.study.commons.constants.board.SkinType;
 import org.study.commons.constants.board.ViewType;
 import org.study.controllers.admin.board.BoardConfig;
+import org.study.controllers.admin.category.CategoryForm;
 import org.study.entities.board.Board;
 import org.study.repositories.board.BoardRepository;
 
@@ -21,23 +23,36 @@ public class BoardConfigService {
     private BoardConfigValidator validator;
 
     public void config(BoardConfig config) {
-        validator.check(config);
+        config(config, null);
+    }
 
+    public void config(BoardConfig config, Errors errors) {
+        if (errors != null && errors.hasErrors()) {
+            return;
+        }
+
+        validator.check(config, errors);
+
+        /**
+         * 엔티티가 이미 등록되어 있으면 기존 엔티티 가져오고(수정)
+         * 없다면 새로운 엔티티로 변환 BoardConfig.of(config);(생성)
+         *
+         */
         String bId = config.getBId();
-        Board board1 = null;
-        if(repository.exists(bId)) { // 이미 등록된 게시판 ID가 있다면
-            board1 = repository.findById(bId).orElseGet(() -> BoardConfig.of(config));
-            board1.setBId(bId);
-            board1.setBoardNm(config.getBoardNm());
-            board1.setRowsPerPage(config.getRowsPerPage());
-            board1.setSkin(SkinType.DEFAULT);
+        Board board = null;
+        if(bId != null && repository.exists(bId)) { // 이미 등록된 게시판 ID가 있다면
+            board = repository.findById(bId).orElseGet(() -> BoardConfig.of(config));
+            board.setBId(bId);
+            board.setBoardNm(config.getBoardNm());
+            board.setRowsPerPage(config.getRowsPerPage());
+            board.setSkin(SkinType.DEFAULT);
             /** 기본 값으로 설정이 맞는지?? */
         }
 
-        if(board1 == null) { // 게시판 ID가 없다면 boardConfig -> Board 엔티티로 변환
-            board1 = BoardConfig.of(config);
+        if(board == null) { // 게시판 ID가 없다면 boardConfig -> Board 엔티티로 변환
+            board = BoardConfig.of(config);
         }
 
-        repository.saveAndFlush(board1);
+        repository.saveAndFlush(board);
     }
 }
