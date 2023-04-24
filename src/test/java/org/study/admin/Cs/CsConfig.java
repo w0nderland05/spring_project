0ccf4952721811d5267a7c739dfc01d9d0e1fb87
bambbang2 <4900ekrud@naver.com>
2023-04-24 12:11:36 +0900

    ddd

--- src/test/java/org/study/admin/Cs/CsConfig.java
+++ src/test/java/org/study/admin/Cs/CsConfig.java
@@ -1,23 +1,27 @@
-package org.study.controllers.admin.cs;
+package org.study.admin.Cs;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.study.commons.constants.ReportStatus;
-import org.study.entities.User;
 
 @Data
 @Builder @AllArgsConstructor @NoArgsConstructor
 public class CsConfig {
-
+    
     private String division;
 
     private Long code;
 
     private String detail;
 
+    private ReportStatus status = ReportStatus.READY;
+
     private String process;
+
+
 
 
 }
