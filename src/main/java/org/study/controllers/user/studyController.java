7f8f6bdabd23a446e5c583ac403052102461b562
김뚜루 <111275210+w0nderland05@users.noreply.github.com>
2023-04-27 00:41:21 +0900

    스터디함께해요 프론트 컨트롤러 및 html추가

---
+++ src/main/java/org/study/controllers/user/studyController.java
@@ -0,0 +1,20 @@
+package org.study.controllers.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user/study")
+public class studyController {
+
+    @GetMapping("/register")
+    public String studyReg() {
+        return "/front/study/register";
+    }
+
+    @GetMapping("/join")
+    public String studyJoin() {
+        return "/front/study/join";
+    }
+}
