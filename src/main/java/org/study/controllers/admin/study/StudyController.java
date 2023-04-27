84e72033ed79c4fda81637bd3eb19aac6ea17614
김뚜루 <111275210+w0nderland05@users.noreply.github.com>
2023-04-28 02:25:10 +0900

    컨트롤러 구현을위해  apply service/ validator 위치 이동

--- src/main/java/org/study/controllers/admin/study/StudyController.java
+++ src/main/java/org/study/controllers/admin/study/StudyController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -29,7 +30,7 @@ public class StudyController {
      *
      * @return
      */
-    @GetMapping("/approvals")
+    @PostMapping("/approvals")
     public String approvals(){
 
         return "admin/study/approvals";
@@ -46,6 +47,8 @@ public class StudyController {
     public String approve(){
         return "admin/study/approve";
     }
+
+
 
 
 
