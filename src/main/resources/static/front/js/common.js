21cb7ace85265618f8052def18febad93695e49e
yonggyo1125 <yonggyo00@kakao.com>
2023-04-29 21:50:03 +0900

    ajax 범용 기능 및 지역 조회 기능

--- src/main/resources/static/front/js/common.js
+++ src/main/resources/static/front/js/common.js
@@ -6,14 +6,73 @@ const commonLib = {
     * @param url - 요청 메서드
     * @method - 요청방식
     * @params - body 요청 데이터
-    * @contentType - 전송 데이터 종류
+    * @type - 전송 데이터 종류
     */
-    ajaxLoad(url, method, params, contentType) {
-        const xhr = new XMLHttpRequest();
-        
+    ajaxLoad(url, method, params, type) {
+        return new Promise((resolve, reject) => {
+            method = method || "GET";
+            const xhr = new XMLHttpRequest();
+            xhr.open(method, url);
+
+            const csrfEl = document.querySelector("meta[name='_csrf']")
+            const csrfHeaderEl = document.querySelector("meta[name='_csrf_header']");
+            if (csrfEl && csrfHeaderEl) {
+                xhr.setRequestHeader(csrfHeaderEl.content, csrfEl.content);
+            }
+
+            if (method && ['POST', 'PUT', 'PATCH'].indexOf(method) != -1) {
+                type =  type.toUpperCase();
+                const contentType = type == 'JSON'? 'application/json':'application/x-www-form-urlencoded';
+                xhr.setRequestHeader('content-type', contentType);
+                if (type != 'JSON') {
+                    params = new URLSearchParams(params).toString();
+                }
+            } else { // GET, DELETE
+                params = null;
+            }
+
+
+           xhr.send(params);
+
+           xhr.onreadystatechange = () => {
+                if (xhr.status == 200 && xhr.readyState == XMLHttpRequest.DONE) {
+                    resolve(xhr.responseText);
+                }
+           };
+
+           xhr.onerror = (err) => {
+                reject(err);
+           };
+
+           xhr.onabort = (err) => {
+                reject(err);
+           };
+        });
+    },
+    /**
+    * 시도 목록
+    *
+    */
+    getSido() {
+
+    },
+    /**
+    * 시구군 조회
+    *
+    */
+    async getSigugun(sido) {
+        let result = [];
+        try {
+            const data = await commonLib.ajaxLoad("/ajax/sigugun/" + sido);
+            if (data) result = data;
+        } catch (err) {
+            console.log(err);
+        }
+
+        return result;
     }
 };
 
 window.addEventListener("DOMContentLoaded", function() {
-
+    
 });
