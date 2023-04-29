46869fd5d8183e264ebaac2ffe34bd7f3a863085
yonggyo1125 <yonggyo00@kakao.com>
2023-04-29 22:18:52 +0900

    지역 관련 공통 소스 개발 완료

--- src/main/resources/static/front/js/common.js
+++ src/main/resources/static/front/js/common.js
@@ -64,7 +64,7 @@ const commonLib = {
         let result = [];
         try {
             const data = await commonLib.ajaxLoad("/ajax/sigugun/" + sido);
-            if (data) result = data;
+            if (data) result = JSON.parse(data);
         } catch (err) {
             console.log(err);
         }
@@ -74,5 +74,32 @@ const commonLib = {
 };
 
 window.addEventListener("DOMContentLoaded", function() {
-    
+    /** 시도 변경시 시구군 데이터 조회 및 반영 처리 S */
+    const ajaxSidoEls = document.getElementsByClassName("ajax_sido");
+    if (ajaxSidoEls.length > 0) {
+        for (const el of ajaxSidoEls) {
+            el.addEventListener("change", async function() {
+                const parentEl = this.parentElement;
+                const targetEl = parentEl.querySelector(".ajax_gigugun");
+                if (!targetEl) return;
+                const sido = this.value.trim();
+                targetEl.innerHTML = "<option value=''>- 시구군 선택-</option>";
+                if (!sido) return;
+
+                let siguguns = await commonLib.getSigugun(sido);
+                if (!siguguns) return;
+                targetEl.innerHTML = "";
+                for (const sgg of siguguns) {
+                    const option = document.createElement("option");
+                    option.value = sgg;
+                    const optionText = document.createTextNode(sgg);
+                    option.appendChild(optionText);
+
+                    targetEl.appendChild(option);
+                }
+
+            });
+        }
+    }
+    /** 시도 변경시 시구군 데이터 조회 및 반영 처리 E */
 });
