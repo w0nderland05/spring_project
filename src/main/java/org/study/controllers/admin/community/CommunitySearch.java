f9e3f585418633a99b1f8d5dab34d8083df1a162
7aeIn96 <xodls713@naver.com>
2023-05-09 19:45:58 +0900

    게시글 조회 수정중

--- src/main/java/org/study/controllers/admin/community/CommunitySearch.java
+++ src/main/java/org/study/controllers/admin/community/CommunitySearch.java
@@ -1,4 +1,18 @@
 package org.study.controllers.admin.community;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder @NoArgsConstructor @AllArgsConstructor
 public class CommunitySearch {
+
+    private int page = 1;
+    private int limit = 20;
+    private Long gid;
+    private String poster;
+    private String subject;
+    private String content;
 }
--- src/main/java/org/study/controllers/user/board/UserBoardController.java
+++ src/main/java/org/study/controllers/user/board/UserBoardController.java
@@ -3,7 +3,6 @@ package org.study.controllers.user.board;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-// 클래스 이름이 같아서 여 부분 수정 해줘야 해요
 @Controller
 @RequestMapping("/user/community")
 public class UserBoardController {
