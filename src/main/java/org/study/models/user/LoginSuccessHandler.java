55e9cc7a927b04d0ba07a7e67b4bafa6119db925
yonggyo1125 <yonggyo00@kakao.com>
2023-05-09 15:43:14 +0900

    로그인 개선

--- src/main/java/org/study/models/user/LoginSuccessHandler.java
+++ src/main/java/org/study/models/user/LoginSuccessHandler.java
@@ -3,6 +3,7 @@ package org.study.models.user;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -11,6 +12,15 @@ import java.io.IOException;
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        HttpSession session = request.getSession();
+        session.removeAttribute("requiredUserEmail");
+        session.removeAttribute("requiredUserPw");
+        session.removeAttribute("loginFail");
+        session.removeAttribute("userEmail");
+
+        /** 회원 정보 편의상 세션에 유지 */
+        UserInfo userInfo = (UserInfo)authentication.getPrincipal();
+        session.setAttribute("userInfo", userInfo);
 
 
         String url = request.getContextPath() + "/index";
